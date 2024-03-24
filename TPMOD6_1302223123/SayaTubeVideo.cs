cd7b489f9bdab0f045f36b72845123746b9f5297
unknown <farisazhar.0241@gmail.com>
2024-03-24 23:33:10 +0700

    inisiasi kedua

--- TPMOD6_1302223123/SayaTubeVideo.cs
+++ TPMOD6_1302223123/SayaTubeVideo.cs
@@ -11,5 +11,39 @@ public class SayaTubeVideo
     public SayaTubeVideo(string title)
     {
 
+
+        this.id = GenerateId();
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    private int GenerateId()
+    {
+        Random IdRandom = new Random();
+        return IdRandom.Next(17064, 99999);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+
+
+        try
+        {
+            checked
+            {
+                playCount = playCount + count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Penambahan pada play count melebihi batas maksimal.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
     }
 }
