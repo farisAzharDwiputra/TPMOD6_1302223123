7775b2162d4231dab26bdc78378151b62e1101c5
unknown <farisazhar.0241@gmail.com>
2024-03-24 23:31:26 +0700

    inisiasi pertama

---
+++ TPMOD6_1302223123/SayaTubeVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+
+    }
+}
